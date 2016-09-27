b27508685b4c89ef52e54cbaabbbcdc49f8c1467
Juan Pablo Escobar <juannpablo@gmail.com>
2016-09-26 20:03:47 -0500

    Adds solution 1_people week 4

--- Semana4/1_people.rb
+++ Semana4/1_people.rb
@@ -1,2 +1,19 @@
 # Escribe un programa que guarde en un arreglo los nombres de todas las personas de la clase solicitando los nombres de teclado
 
+# Declaramos el nombre del nuevo arreglo
+clase = []
+
+puts "¿Cuántas personas hay en la clase?"
+cantidad_de_personas = gets.to_i
+
+# Iniciamos un ciclo con la cantidad de personas en la clase
+cantidad_de_personas.times do |contador|
+  puts "Pásame el nombre #{contador+1}"
+
+  # Empujamos el nuevo valor solicitado al usuario a nuestro arreglo clase
+  clase.push(gets.chomp)
+end
+
+puts "Esta es la lista: "
+# Imprimimos nuestro arreglo
+puts clase
