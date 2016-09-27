b5d3093778ce6de5d847bbb8676139c80cd93d96
Juan Pablo Escobar <juannpablo@gmail.com>
2016-09-26 21:23:28 -0500

    Adds 2_ratings solution week 4

--- Semana4/2_ratings.rb
+++ Semana4/2_ratings.rb
@@ -2,3 +2,36 @@
 # y regresa la calificación promedio en un objeto float.
 # Si el promedio está arriba de 4.0 imprime que la película fue un éxito
 # Hint! -> rand(n) te da un número random del 0 a n
+#
+
+# Declarar el nombre del arreglo
+ratings = []
+
+# Guardamos en una variable cuantas personas son
+puts "¿Cuántas personas evaluaron?"
+numero_de_personas = gets.to_i
+
+# Abrimos un ciclo que nos ayude a guardar los ratings en nuestro arreglo
+numero_de_personas.times do
+  ratings.push(rand(0..5))
+end
+
+# Creas una variable sumatoria
+sumatoria = 0
+
+# Abrimos un nuevo ciclo y sumamos cada valor dentro del arreglo en sumatoria
+puts "Los ratings generados son:"
+ratings.each_with_index do |rating, index|
+  puts "| #{index} | #{rating} |"
+  sumatoria += rating # sumatoria = sumatoria + rating
+end
+puts "La sumatoria es: #{sumatoria}"
+
+# Obtenemos el promedio de ratings dividiendo la sumatoria entre la cantidad de personas
+promedio = 0.0
+promedio = sumatoria / numero_de_personas # TO-DO: Lograr que haga la división como float
+
+puts "El promedio es: #{promedio}"
+
+# Si promedio es >= 4 imprimo "La película fue un éxito!"
+puts "La película fue un éxito" if promedio >= 4.0
