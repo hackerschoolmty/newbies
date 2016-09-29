d43911ed8e46b210f97ee964cf13f6db96c5dfdc
Juan Pablo Escobar <juannpablo@gmail.com>
2016-09-28 20:56:16 -0500

    Adds solutions to 3 and 5 arrays exercises

--- Semana4/3_reverse.rb
+++ Semana4/3_reverse.rb
@@ -4,3 +4,22 @@
 # #=> names_reversed = ["Annie", "Rafa", "Kuri", "Edo", "JP"]
 
 # Escribe tu código aquí
+
+names = ["JP", "Edo", "Kuri", "Rafa", "Annie"]
+
+puts "Arreglo original: "
+puts names.join(", ") # .join une los elementos de array en un string
+
+# Declaramos el nuevo arreglo que tendrá el reverso del anterior
+names_reversed = []
+
+# Recorrer el arreglo names iniciando al final del arreglo
+cont = names.length - 1
+
+while cont >=0 do
+  names_reversed.push(names[cont])
+  cont -= 1
+end
+
+puts "Arreglo en orden al revés: "
+puts names_reversed.join(", ")
--- Semana4/5_sorting.rb
+++ Semana4/5_sorting.rb
@@ -1,1 +1,48 @@
 # Genera un arreglo con N números random del 1 al 20 y regresa el mismo arreglo ordenado del menor al mayor
+
+# Tu código aquí
+
+# Solicitar al usuario cuantos números quisiera generar dentro del arreglo
+puts "¿Cuántos elementos deseas en el arreglo?"
+cantidad_de_elementos = gets.to_i
+
+# Declaramos el arreglo de números
+numeros = []
+
+# Llenar el arreglo con N número random del 1 al 20
+cantidad_de_elementos.times do
+  numeros.push(rand(1..20))
+end
+
+# Imprimir el arreglo generado
+puts numeros
+
+# Ordenar el arreglo con sus elementos del menor al mayor
+
+cantidad_de_elementos.times do
+  a = 0
+  b = 1
+  cont = 0
+  temp = 0
+  while cont < numeros.length do
+    # Si el elemento que está en A es mayor que el elemento que está en B
+    puts numeros.join("|")
+    if numeros[b] != nil and numeros[a] > numeros[b]
+      # Hacemos el swap
+      temp = numeros[a]
+      numeros[a] = numeros[b]
+      numeros[b] = temp
+    end
+    a += 1
+    b += 1
+    cont += 1
+  end
+  puts "Concluye ciclo"
+end
+
+puts "Nuestro arreglo ordenado del menor al mayor"
+puts numeros
+
+
+
+
