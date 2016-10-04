c8b4dc792fec6829f1d0f195b11bc87e3d4013d8
edolopez <elopez.deleon@gmail.com>
2016-10-04 07:37:05 -0500

    Answers Week4 / 10_left_shifted.rb

--- Semana4/10_left_shifted.rb
+++ Semana4/10_left_shifted.rb
@@ -1,6 +1,26 @@
-# Return an array that is "left_shifted" by one -- so [6,2,5,3] prints [2,5,3,6]. 
+# Print an array that is "left_shifted" by one -- so [6,2,5,3] prints [2,5,3,6]. 
 
 # You may modify and return the given array, or return a new array.
 # [6, 2, 5, 3] → [2, 5, 3, 6] 
 # [1, 2] → [2, 1] 
 # [1] → [1]
+
+array = [6, 2, 5, 3]
+puts array.join(' ')
+
+# Rubyist way
+# number = array[0]
+# array.delete_at(0)
+# array.push(number)
+
+# Hardcore Developer Way
+number = array[0]
+index = 1
+while index < (array.length)
+	array[index - 1] = array[index]
+	index = index + 1
+end
+array[index - 1] = number
+# array[array.length - 1] = number
+
+puts array.join(' ')
--- Semana4/9_triple_up.rb
+++ Semana4/9_triple_up.rb
@@ -1,4 +1,4 @@
-# Return true if the array contains, somewhere, three increasing adjacent numbers like 4, 5, 6 or 23, 24, 25.
+# Print true if the array contains, somewhere, three increasing adjacent numbers like 4, 5, 6 or 23, 24, 25.
 
 # [1, 4, 5, 6, 2] → true 
 # [1, 2, 3] → true 
