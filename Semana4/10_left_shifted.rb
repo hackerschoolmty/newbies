d2382dbb4b48741cba8cc3433830d0bcbff5ae15
edolopez <elopez.deleon@gmail.com>
2016-10-03 20:49:40 -0500

    Adds missing exercises

---
+++ Semana4/10_left_shifted.rb
@@ -0,0 +1,6 @@
+# Return an array that is "left_shifted" by one -- so [6,2,5,3] prints [2,5,3,6]. 
+
+# You may modify and return the given array, or return a new array.
+# [6, 2, 5, 3] → [2, 5, 3, 6] 
+# [1, 2] → [2, 1] 
+# [1] → [1]
---
+++ Semana4/9_triple_up.rb
@@ -0,0 +1,5 @@
+# Return true if the array contains, somewhere, three increasing adjacent numbers like 4, 5, 6 or 23, 24, 25.
+
+# [1, 4, 5, 6, 2] → true 
+# [1, 2, 3] → true 
+# [1, 2, 4] → false
