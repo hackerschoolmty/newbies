c191be98ba29bd1a248b51e802a96e9f6b23f3d4
edolopez <elopez.deleon@gmail.com>
2016-10-05 20:00:03 -0500

    Adds Week5 exercises

---
+++ Semana5/2_1_languages.rb
@@ -0,0 +1,15 @@
+languages = {
+  :de => 'German',
+  :en => 'English',
+  :es => 'Spanish',
+}
+dictionary = {
+  :de => { :one => 'eins', :two => 'zwei', :three => 'drei' },
+  :en => { :one => 'one', :two => 'two', :three => 'three' },
+  :es => { :one => 'uno', :two => 'dos', :three => 'tres' }
+}
+
+# 2.1 Add code in order to print the next lines of code
+
+# => In German, eins means one, zwei means two, drei means three.
+# => In Spanish, uno means one, duo means two, tres means three.
---
+++ Semana5/2_languages.rb
@@ -0,0 +1,16 @@
+languages = {
+  :de => 'German',
+  :en => 'English',
+  :es => 'Spanish',
+}
+dictionary = {
+  :de => { :one => 'eins', :two => 'zwei', :three => 'drei' },
+  :en => { :one => 'one', :two => 'two', :three => 'three' },
+  :es => { :one => 'uno', :two => 'dos', :three => 'tres' }
+}
+
+# 2.2 Add code in order to print the next lines of code
+
+# => de eins zwei drei
+# => en one two three
+# => es uno dos tres
