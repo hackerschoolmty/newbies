54ba15d13b9f7f1b90d5ffb69ad2756bee06b7e9
Juan Pablo Escobar <juannpablo@gmail.com>
2016-10-08 10:52:30 -0500

    Adds 3_interviews exercise

---
+++ Semana5/3_interviews.rb
@@ -0,0 +1,44 @@
+# Escribe un programa que defina a los entrevistadores para cada candidato
+# Consideraciones para tu programa:
+# 1. 2 entrevistadores random para cada candidato
+# 2. Cuando se seleccionen los entrevistadores envía un correo al candidato
+## Ejemplo output:
+## Enviando correo a: hacker1@gmail.com
+## Hola Jesus Lerma
+## Tus entrevistadores son Edo y Rafa
+
+INTERVIEWERS = [
+  {name: 'Adrian', email: 'adrian@hsmty.mx'},
+  {name: 'Kuri', email: 'kuri@hsmty.mx'},
+  {name: 'JP', email: 'jp@hsmty.mx'},
+  {name: 'Edo', email: 'edo@hstmty.mx'},
+  {name: 'Rafa', email: 'Rafa@hsmty.mx'}
+]
+
+CANDIDATES = [
+  {:name=>"Jesus Lerma", :email=>"hacker1@gmail.com"}, 
+  {:name=>"Alejandro Sandoval", :email=>"hacker2@gmail.com"}, 
+  {:name=>"Eduardo Romero", :email=>"hacker3@gmail.com"}, 
+  {:name=>"Benjamin Moreno", :email=>"hacker4@gmail.com"}, 
+  {:name=>"William Alvarez", :email=>"hacker5@gmail.com"}, 
+  {:name=>"Pablo De Jess Garca", :email=>"hacker6@gmail.com"}, 
+  {:name=>"Luis Arturo Cerda", :email=>"hacker7@gmail.com"}, 
+  {:name=>"Rodrigo Camargo", :email=>"hacker8@gmail.com"}, 
+  {:name=>"Andres Garza", :email=>"hacker9@gmail.com"}, 
+  {:name=>"Josue Zavala", :email=>"hacker10@gmail.com"}, 
+  {:name=>"Juan Garcia", :email=>"hacker11@gmail.com"}, 
+  {:name=>"Yaser Almasri", :email=>"hacker12@gmail.com"}, 
+  {:name=>"Omar Manjarrez", :email=>"hacker13@gmail.com"}, 
+  {:name=>"Esteban Lara", :email=>"hacker14@gmail.com"}, 
+  {:name=>"Daniel Brambila", :email=>"hacker15@gmail.com"}, 
+  {:name=>"Jose Contreras", :email=>"hacker16@gmail.com"}, 
+  {:name=>"Gustavo Prez", :email=>"hacker17@gmail.com"}, 
+  {:name=>"Eduardo Williams", :email=>"hacker18@gmail.com"}, 
+  {:name=>"Oliver Mendoza", :email=>"hacker19@gmail.com"}, 
+  {:name=>"Angel Montoya", :email=>"hacker20@gmail.com"}, 
+  {:name=>"Luis Castillo", :email=>"hacker21@gmail.com"}, 
+  {:name=>"Daniel Brambila", :email=>"hacker22@gmail.com"}
+]
+
+# Code
+
