1245c945b2c830d502c991a56272c13275f01413
Rafael Cardenas <rafael@rafaelcr.com>
2016-10-11 20:10:42 -0500

    Primera mitad resuelta.

--- Semana6/7_busqueda.rb
+++ Semana6/7_busqueda.rb
@@ -7,5 +7,14 @@
 #   busqueda(5, [2, 4, 6, 8]) => false
 
 def busqueda(numero_a_buscar, arreglo)
-
+  arreglo.each do |numero|
+    if numero == numero_a_buscar
+      return true
+    end
+  end
+  return false
 end
+
+puts busqueda(5, [2, 4, 6, 8])
+puts busqueda(3, [1, 2, 3])
+puts busqueda("b", ["a", "b", "c"])
--- Semana6/8_combina_arreglos.rb
+++ Semana6/8_combina_arreglos.rb
@@ -5,3 +5,17 @@
 #
 # Ejemplo:
 #   combina(["a", "b", "c"], [1, 2, 3]) => ["a", 1, "b", 2, "c", 3]
+
+def combina(arreglo_1, arreglo_2)
+  arreglo_3 = []
+  i = 0
+  while i < arreglo_1.length
+    arreglo_3.push(arreglo_1[i])
+    arreglo_3.push(arreglo_2[i])
+    i += 1
+  end
+  return arreglo_3
+end
+
+puts combina([1, 2, 3], ["a", "b", "c"])
+puts combina(["hola", "como", "estas?"], ["bien", "y", "tu?"])
